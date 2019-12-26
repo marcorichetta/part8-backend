09624e5a781b5eccd2738ca3054ca18f70d4c417
Marco Richetta <marcorichetta@gmail.com>
2019-12-26 16:10:06 -0300

    8.15 - Validation

--- index.js
+++ index.js
@@ -22,63 +22,6 @@ mongoose.connect(MONGO_PROD_URI, { useNewUrlParser: true })
         console.log('Error on connection to MongoDB:', error.message)
     })
 
-/*
- * It would be more sensible to associate book and the author by saving 
- * the author id instead of the name to the book.
- * For simplicity we, however, save the author name.
-*/
-let books = [
-    {
-        title: 'Clean Code',
-        published: 2008,
-        author: 'Robert Martin',
-        id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Agile software development',
-        published: 2002,
-        author: 'Robert Martin',
-        id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-        genres: ['agile', 'patterns', 'design']
-    },
-    {
-        title: 'Refactoring, edition 2',
-        published: 2018,
-        author: 'Martin Fowler',
-        id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring']
-    },
-    {
-        title: 'Refactoring to patterns',
-        published: 2008,
-        author: 'Joshua Kerievsky',
-        id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'patterns']
-    },
-    {
-        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-        published: 2012,
-        author: 'Sandi Metz',
-        id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-        genres: ['refactoring', 'design']
-    },
-    {
-        title: 'Crime and punishment',
-        published: 1866,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'crime']
-    },
-    {
-        title: 'The Demon',
-        published: 1872,
-        author: 'Fyodor Dostoevsky',
-        id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-        genres: ['classic', 'revolution']
-    },
-]
-
 const typeDefs = gql`
 
     type Author {
@@ -161,7 +104,7 @@ const resolvers = {
 
             let authors = await Author.find({})
 
-            
+
             // Calculate the book count for each author
             const authorsPromises = authors.map(async a => {
 
@@ -171,7 +114,7 @@ const resolvers = {
                 // Modify the book count for each author
                 a.bookCount = authorBooks.length
             })
-            
+
             /* 
                 Cada map crea su propia operación async
                 AllAuthors no espera a que terminen de ejecutarse
@@ -201,14 +144,16 @@ const resolvers = {
                     // Guardarlo en el backend
                     await author.save()
                 } catch (error) {
+
                     throw new UserInputError(error.message, {
                         invalidArgs: args
                     })
+
                 }
-            } 
-            
+            }
+
             // Crear el libro con el autor encontrado o creado
-            const newBook = new Book({ 
+            const newBook = new Book({
                 ...args,
                 author: author
             })
@@ -221,8 +166,6 @@ const resolvers = {
                     invalidArgs: args
                 })
             }
-        
-    
 
             return newBook
         },
@@ -235,7 +178,7 @@ const resolvers = {
             }
 
             // Update the author data
-            author.born = args.setBornTo 
+            author.born = args.setBornTo
 
             // Replace it in the list
             try {
